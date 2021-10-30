8d94984bedf363923c37d7ffd0c61e04f5c4576d
hzoroofc <houman@houman-zoroofchi.de>
2021-10-30 11:08:38 +0200

    Comit mit Rudolf

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -14,6 +14,7 @@ public class Cinema {
         getPriceOfTicket(rowNumber, rowsCount, seatsInEachRowCount);
         bookedTheSeat(cinemaRoom, rowNumber, seatNumber);
         printCinema(cinemaRoom, seatsInEachRowCount);
+        System.out.println(1);
     }
 
     public static int getRowsCount() {
